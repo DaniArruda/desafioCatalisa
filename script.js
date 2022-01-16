8534b84852ed608fd5507fa9b085706cbae392b3
daniaarruda <arruda.danielealves@gmail.com>
2022-01-16 15:26:51 -0300

    adicionar parametros para captura dos personagens e informacoes

--- script.js
+++ script.js
@@ -3,3 +3,31 @@ const botao = document.querySelector('button');
 const nomeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
 const condicao = document.querySelector('#status');
+
+traduzirCondicao = (data) => {
+    if(data.status == 'unknown'){
+        return 'Não sabemos';
+    }else if(data.status == 'Alive'){
+        return 'Sim';
+    }else {
+        return 'Não. Está morto';
+    }
+}
+
+pegarPersonagem = () => {
+    return fetch(`https://rickandmortyapi.com/api/character/`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((Response) => Response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt = data.name;
+        nomeDoPersonagem.innerHTML = data.name;
+        especie.innerHTML = data.species;
+        condicao.innerHTML = traduzirCondicao(data);
+    });
+}
+
+botao.onclick = pegarPersonagem;
